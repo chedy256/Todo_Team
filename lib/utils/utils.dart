9aaab4a135859889b8a82c7b5fcc9f5098a01d50
chedy amine el haj <chedyelhaj@gmail.com>
2025-08-18 18:44:58 +0100

    Added time left calculator utility

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,1 +1,17 @@
 //TODO json parse
+
+class Utils {
+  static String timeLeft(DateTime dueDate) {
+    final now = DateTime.now();
+    final difference = dueDate.difference(now);
+
+    if (difference.isNegative) {
+      return 'Expiré';
+    } else if (difference.inDays > 0) {
+      return '${difference.inDays} jour${difference.inDays > 1 ? 's' : ''}';
+    } else if (difference.inHours > 0) {
+      return '${difference.inHours} heure${difference.inHours > 1 ? 's' : ''}';
+    }
+    return '${difference.inMinutes} minute${difference.inMinutes > 1 ? 's' : ''}';
+  }
+}
